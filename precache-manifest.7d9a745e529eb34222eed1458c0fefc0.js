8520309c164352a0cff02ddd01c644c980771efb
therealsharath <palathingal@gatech.edu>
2020-05-06 06:07:40 -0400

    Updates

---
+++ precache-manifest.7d9a745e529eb34222eed1458c0fefc0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46dc5dfe0dd82ccf6e2b705ca6493c75",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "467166102bbe65d66702",
+    "url": "/portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3958defbe8c8891ab270",
+    "url": "/portfolio/static/css/main.56914297.chunk.css"
+  },
+  {
+    "revision": "467166102bbe65d66702",
+    "url": "/portfolio/static/js/2.96266b1d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.96266b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3958defbe8c8891ab270",
+    "url": "/portfolio/static/js/main.6b6e841b.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "ca38b2147e0ade5cde0e0ad9110cd9a1",
+    "url": "/portfolio/static/media/Headshot.ca38b214.jpeg"
+  },
+  {
+    "revision": "17e0dd66dd975d2b47292a652e2be395",
+    "url": "/portfolio/static/media/bgVideo.17e0dd66.mp4"
+  },
+  {
+    "revision": "b5c2a92912da2d317864961c70f010fa",
+    "url": "/portfolio/static/media/mainVideo.b5c2a929.mp4"
+  },
+  {
+    "revision": "b0bddd1dba80a092c3175c3707655da6",
+    "url": "/portfolio/static/media/storyVid.b0bddd1d.mp4"
+  }
+]);
